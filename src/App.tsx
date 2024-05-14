a90f3d54fc5f1c9c2233701236486bcfe8d71fa3
Andrew Mukare Ngumbao <dremukare@gmail.com>
2024-05-14 15:14:08 +0300

    wip: building out project pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,45 @@
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-import LandingLayout from './components/layout/LandingLayout';
-import GalleryLandingPage from './components/pages/GalleryLandingPage';
-import AllProjects from './components/projects/AllProjects';
-import EcommerceProjects from './components/projects/EcommerceProjects';
-import BusinessSolutionProjects from './components/projects/BusinessSolutionProjects';
-import FAQPage from './components/pages/FAQPage';
-import ProjectsLayout from './components/layout/ProjectsLayout';
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import LandingLayout from "./components/layout/LandingLayout";
+import GalleryLandingPage from "./components/pages/GalleryLandingPage";
+import AllProjects from "./components/projects/AllProjects";
+import FAQPage from "./components/pages/FAQPage";
+import ProjectsLayout from "./components/layout/ProjectsLayout";
 
 function App() {
-	const router = createBrowserRouter([
-		{
-			path: '/',
-			element: <LandingLayout />,
-			children: [
-				{
-					path: '/',
-					element: <GalleryLandingPage />,
-				},
-				{
-					path: '/faq',
-					element: <FAQPage />,
-				},
-			],
-		},
-		{
-			path: '/projects',
-			element: <ProjectsLayout />,
-			children: [
-				{
-					path: '/projects',
-					element: <AllProjects />,
-				},
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <LandingLayout />,
+      children: [
+        {
+          path: "/",
+          element: <GalleryLandingPage />,
+        },
+        {
+          path: "/faq",
+          element: <FAQPage />,
+        },
+      ],
+    },
+    {
+      path: "/projects",
+      element: <ProjectsLayout />,
+      children: [
+        {
+          path: "/projects",
+          element: <AllProjects />,
+        },
+        // TODO: import project page template and pass relevant props
+        // { path: "/projects/e-commerce-projects", element: <EcommerceProjects /> },
+        // {
+        //   path: "/projects/business-solution-projects",
+        //   element: <BusinessSolutionProjects />,
+        // },
+      ],
+    },
+  ]);
 
-				{ path: '/projects/e-commerce', element: <EcommerceProjects /> },
-				{
-					path: '/projects/business-solutions',
-					element: <BusinessSolutionProjects />,
-				},
-			],
-		},
-	]);
-
-	return <RouterProvider router={router} />;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ src/components/pages/TemplateProjectPage.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TemplateProjectPage = () => {
+  return <div>TemplateProjectPage</div>;
+};
+
+export default TemplateProjectPage;
