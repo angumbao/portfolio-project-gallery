956d2cf223751ea5209c6e5092698966fcde16f6
Andrew Mukare Ngumbao <dremukare@gmail.com>
2024-05-14 18:01:53 +0300

    wip: creating project pages

--- src/components/projects/AllProjects.tsx
+++ src/components/projects/AllProjects.tsx
@@ -1,5 +1,49 @@
+import { Box, Space, Text, Title } from "@mantine/core";
+import ProjectPageHeroText from "./ProjectPageHeroText";
+import { Carousel } from "@mantine/carousel";
+
+const ProjectSection = (props: {
+  title: string;
+  description?: string;
+  projects: {
+    teamMembers: string[];
+    title: string;
+    technologies: string[];
+    videoUrl: string;
+  }[];
+}) => {
+  const { title, description, projects } = props;
+
+  return (
+    <Box>
+      <Title order={3} my="lg">
+        {title}
+      </Title>
+      {description && <Text>{description}</Text>}
+      <Carousel>
+        {projects.map((project, key) => {
+          return <Carousel.Slide key={key}></Carousel.Slide>;
+        })}
+      </Carousel>
+    </Box>
+  );
+};
+
 const AllProjects = () => {
-	return <div>AllProjects</div>;
+  return (
+    <div>
+      <Space h="xl" />
+      <Space h="xl" />
+      <Space h="xl" />
+      <ProjectPageHeroText text={"ALL PORTFOLIO PROJECTS"} />
+      <Text ta={"center"} lh="1.6em" fz="xl" fw="400" my={"xl"}>
+        We're thrilled to showcase a handpicked selection of standout projects
+        meticulously created by our graduates. From cutting-edge technologies to
+        visionary designs, you'll gain invaluable insights into the caliber and
+        potential of our graduates making real world impact with their skills.
+      </Text>
+    </div>
+  );
 };
 
 export default AllProjects;
