01dca19c72d644118d9a9447510080e037465d41
Andrew Mukare Ngumbao <dremukare@gmail.com>
2024-05-14 17:44:49 +0300

    minor: improved projects layout

--- src/components/layout/ProjectsLayout.tsx
+++ src/components/layout/ProjectsLayout.tsx
@@ -2,12 +2,17 @@ import { Outlet } from "react-router-dom";
 import NavBar from "../common/NavBar";
 import Footer from "../common/Footer";
 import ThemeProvider from "../ThemeProvider";
+import { Box, Container } from "@mantine/core";
 
 const ProjectsLayout = () => {
   return (
     <ThemeProvider>
-      <NavBar />
-      <Outlet />
+      <Box mih="calc(100vh - 112px)">
+        <NavBar />
+        <Container size={"lg"}>
+          <Outlet />
+        </Container>
+      </Box>
       <Footer />
     </ThemeProvider>
   );
