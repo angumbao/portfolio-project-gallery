e85f19796c42ff08b3780ff036e5122750a0c2e5
Andrew Mukare Ngumbao <dremukare@gmail.com>
2024-05-22 14:09:38 +0300

    feat: modal implemented, card styling improved

--- src/components/projects/ProjectCard.tsx
+++ src/components/projects/ProjectCard.tsx
@@ -11,6 +11,7 @@ import {
   Modal,
   ScrollArea,
   Text,
+  Title,
   Tooltip,
   useMantineTheme,
 } from "@mantine/core";
@@ -19,7 +20,7 @@ import { ParsedProject } from "../../types/Project";
 import VideoThumbnail from "react-video-thumbnail";
 import { IconBrandLinkedin } from "@tabler/icons-react";
 import VideoPlayer from "../common/VideoPlayer";
-import { useDisclosure } from "@mantine/hooks";
+import { useDisclosure, useMediaQuery } from "@mantine/hooks";
 import { motion } from "framer-motion";
 
 const ProjectCard = (props: { project: ParsedProject }) => {
@@ -30,6 +31,7 @@ const ProjectCard = (props: { project: ParsedProject }) => {
   const [opened, { open, close }] = useDisclosure();
 
   const theme = useMantineTheme();
+  const isMobile = useMediaQuery(`(max-width: ${theme.breakpoints.sm})`);
 
   const isZoomVideo = project.video.includes("zoom");
 
@@ -39,122 +41,175 @@ const ProjectCard = (props: { project: ParsedProject }) => {
 
   const isGoogleVideo = project.video.includes("drive.google");
 
+  const TeamMembers = (props: { inModal?: boolean }) => {
+    const { inModal } = props;
+    return (
+      teamMemberNames && (
+        <Box mt="auto" w="100%" my={inModal ? "sm" : ""}>
+          <Text size="sm">Team Members: </Text>
+          <Flex wrap={"wrap"} align={"center"}>
+            {teamMemberNames.map((name, index) => {
+              if (index > 1) return null;
+              return (
+                <Flex w="70%" key={index} mb="0.3em">
+                  <Text size="xs" c="blue.6">
+                    {name}
+                  </Text>
+                  <ActionIcon
+                    color="blue.2"
+                    size={"sm"}
+                    ml="xs"
+                    onClick={() => {
+                      window.open(
+                        project.linkedIn.split("+")[index].replace(" ", ""),
+                        "_blank"
+                      );
+                    }}
+                  >
+                    <IconBrandLinkedin />
+                  </ActionIcon>{" "}
+                </Flex>
+              );
+            })}
+          </Flex>
+        </Box>
+      )
+    );
+  };
+
+  const ProjectTitle = (props: { inModal?: boolean }) => {
+    const { inModal } = props;
+    return inModal ? (
+      <Title order={3} c="blue.7" mb={"md"}>
+        {project.projectName}
+      </Title>
+    ) : (
+      <Flex mt="md" mb="xs" w="100%">
+        <Text w={500}>{project.projectName}</Text>
+      </Flex>
+    );
+  };
+
+  const Technologies = () => (
+    <Flex align="center" wrap="wrap" gap="sm">
+      {project.techonologies &&
+        project.techonologies.split("+").map((technology, idx) => (
+          <Badge key={idx} color="blue.7" variant="light">
+            {technology}
+          </Badge>
+        ))}
+    </Flex>
+  );
+
   return (
     <Box h="auto">
       <Modal
         opened={opened}
         onClose={close}
-        title={project.projectName}
+        title={`${project.projectName} `}
         centered
-        size={"xl"}
+        fullScreen={isMobile}
+        size={isMobile ? "90%" : "60%"}
         overlayProps={{
           color: theme.colors.dark[9],
-          opacity: 0.55,
-          blur: 3,
+          opacity: 1,
+          blur: 5,
         }}
-      ></Modal>
+      >
+        <Box w="100%" h="auto" my={"md"}>
+          {isYoutubeVideo && (
+            <VideoPlayer
+              videoSrc={project.video}
+              videoTitle={project.projectName}
+              width="100%"
+              // maxHeight="30%"
+            />
+          )}
+          {(isLoomVideo || isZoomVideo || isGoogleVideo) && (
+            <iframe
+              src={project.video}
+              allowFullScreen
+              style={{
+                width: "100%",
+                height: "auto",
+                borderRadius: theme.radius.md,
+              }}
+            />
+          )}
+        </Box>
+        <ProjectTitle inModal />
+        <Technologies inModal />
+        {project.projectDescription && (
+          <Text mt="lg" size="sm" c="dimmed" my="sm">
+            {project.projectDescription.split("+").join(", ")}
+          </Text>
+        )}
+        <TeamMembers inModal />
+      </Modal>
       <motion.div
         whileHover={{
           scale: 1.1,
         }}
         style={{
           width: "23em",
-          height: "30em",
+          height: "34em",
         }}
       >
-        <Card
-          // component="button"
-          onClick={() => {
-            //open modal
-            open();
-          }}
-          style={{
-            cursor: "pointer",
-          }}
-          shadow="sm"
-          padding="lg"
-          radius="md"
-          withBorder
-          w="23em"
-          h="30em"
-        >
-          {/* <ScrollArea h="100%" type="hover"> */}
-          <Card.Section mih="55%">
-            {isYoutubeVideo && (
-              <VideoPlayer
-                videoSrc={project.video}
-                videoTitle={project.projectName}
-                width="100%"
-                // maxHeight="30%"
-              />
+        <ScrollArea h="100%" type="hover">
+          <Card
+            // component="button"
+            onClick={() => {
+              //open modal
+              open();
+            }}
+            style={{
+              cursor: "pointer",
+            }}
+            shadow="sm"
+            padding="lg"
+            radius="md"
+            withBorder
+            w="23em"
+            h="30em"
+          >
+            <Card.Section mih="55%" mb="-2em" pb="0">
+              {isYoutubeVideo && (
+                <VideoPlayer
+                  videoSrc={project.video}
+                  videoTitle={project.projectName}
+                  width="100%"
+                  // maxHeight="30%"
+                />
+              )}
+              {(isLoomVideo || isZoomVideo || isGoogleVideo) && (
+                <iframe
+                  src={project.video}
+                  allowFullScreen
+                  style={{
+                    width: "100%",
+                    height: "auto",
+                    borderRadius: theme.radius.md,
+                  }}
+                />
+              )}
+            </Card.Section>
+            <ProjectTitle />
+            <Technologies />
+            {project.projectDescription && (
+              <Tooltip
+                label={project.projectDescription.split("+").join(", ")}
+                withArrow
+                multiline
+                w={420}
+              >
+                <Text truncate mt="lg" size="sm" c="dimmed">
+                  {project.projectDescription.split("+").join(", ")}
+                </Text>
+              </Tooltip>
             )}
-            {(isLoomVideo || isZoomVideo || isGoogleVideo) && (
-              <iframe
-                src={project.video}
-                allowFullScreen
-                style={{
-                  width: "100%",
-                  height: "auto",
-                  borderRadius: theme.radius.md,
-                }}
-              />
-            )}
-          </Card.Section>
-          <Flex mt="md" mb="xs" w="100%">
-            <Text w={500}>{project.projectName ?? "Portfolio Project"}</Text>
-          </Flex>
-          <Flex align="center" wrap="wrap" gap="sm">
-            {project.techonologies &&
-              project.techonologies.split("+").map((technology, idx) => (
-                <Badge key={idx} color="blue.7" variant="light">
-                  {technology}
-                </Badge>
-              ))}
-          </Flex>
-          {project.projectDescription && (
-            <Tooltip
-              label={project.projectDescription.split("+").join(", ")}
-              withArrow
-              multiline
-              w={420}
-            >
-              <Text truncate mt="lg" size="sm" c="dimmed">
-                {project.projectDescription.split("+").join(", ")}
-              </Text>
-            </Tooltip>
-          )}
-          {teamMemberNames && (
-            <Box mt="auto" w="100%">
-              <Text size="sm">Team Members: </Text>
-              <Flex wrap={"wrap"}>
-                {teamMemberNames.map((name, index) => {
-                  if (index > 1) return null;
-                  return (
-                    <Flex w="70%" key={index} mb="0.3em">
-                      <Text size="xs" c="blue.6">
-                        {name}
-                      </Text>
-                      <ActionIcon
-                        color="blue.2"
-                        size={"sm"}
-                        ml="xs"
-                        onClick={() => {
-                          window.open(
-                            project.linkedIn.split("+")[index].replace(" ", ""),
-                            "_blank"
-                          );
-                        }}
-                      >
-                        <IconBrandLinkedin />
-                      </ActionIcon>{" "}
-                    </Flex>
-                  );
-                })}
-              </Flex>
-            </Box>
-          )}
-          {/* </ScrollArea> */}
-        </Card>
+            <TeamMembers />
+          </Card>
+        </ScrollArea>
       </motion.div>
     </Box>
   );
