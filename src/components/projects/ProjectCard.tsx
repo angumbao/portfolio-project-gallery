78c2f7a96a723b46c1aeed79a6922df134fd1608
Andrew Mukare Ngumbao <dremukare@gmail.com>
2024-05-22 13:06:39 +0300

    minor: increasing project card scale on hover

--- src/components/projects/ProjectCard.tsx
+++ src/components/projects/ProjectCard.tsx
@@ -20,6 +20,7 @@ import VideoThumbnail from "react-video-thumbnail";
 import { IconBrandLinkedin } from "@tabler/icons-react";
 import VideoPlayer from "../common/VideoPlayer";
 import { useDisclosure } from "@mantine/hooks";
+import { motion } from "framer-motion";
 
 const ProjectCard = (props: { project: ParsedProject }) => {
   const { project } = props;
@@ -52,94 +53,109 @@ const ProjectCard = (props: { project: ParsedProject }) => {
           blur: 3,
         }}
       ></Modal>
-      <Card
-        // component="button"
-        onClick={() => {
-          //open modal
-          open();
+      <motion.div
+        whileHover={{
+          scale: 1.1,
         }}
         style={{
-          cursor: "pointer",
+          width: "23em",
+          height: "30em",
         }}
-        shadow="sm"
-        padding="lg"
-        radius="md"
-        withBorder
-        w="23em"
-        h="30em"
       >
-        {/* <ScrollArea h="100%" type="hover"> */}
-        <Card.Section mih="55%">
-          {isYoutubeVideo && (
-            <VideoPlayer
-              videoSrc={project.video}
-              videoTitle={project.projectName}
-              width="100%"
-              // maxHeight="30%"
-            />
+        <Card
+          // component="button"
+          onClick={() => {
+            //open modal
+            open();
+          }}
+          style={{
+            cursor: "pointer",
+          }}
+          shadow="sm"
+          padding="lg"
+          radius="md"
+          withBorder
+          w="23em"
+          h="30em"
+        >
+          {/* <ScrollArea h="100%" type="hover"> */}
+          <Card.Section mih="55%">
+            {isYoutubeVideo && (
+              <VideoPlayer
+                videoSrc={project.video}
+                videoTitle={project.projectName}
+                width="100%"
+                // maxHeight="30%"
+              />
+            )}
+            {(isLoomVideo || isZoomVideo || isGoogleVideo) && (
+              <iframe
+                src={project.video}
+                allowFullScreen
+                style={{
+                  width: "100%",
+                  height: "auto",
+                  borderRadius: theme.radius.md,
+                }}
+              />
+            )}
+          </Card.Section>
+          <Flex mt="md" mb="xs" w="100%">
+            <Text w={500}>{project.projectName ?? "Portfolio Project"}</Text>
+          </Flex>
+          <Flex align="center" wrap="wrap" gap="sm">
+            {project.techonologies &&
+              project.techonologies.split("+").map((technology, idx) => (
+                <Badge key={idx} color="blue.7" variant="light">
+                  {technology}
+                </Badge>
+              ))}
+          </Flex>
+          {project.projectDescription && (
+            <Tooltip
+              label={project.projectDescription.split("+").join(", ")}
+              withArrow
+              multiline
+              w={420}
+            >
+              <Text truncate mt="lg" size="sm" c="dimmed">
+                {project.projectDescription.split("+").join(", ")}
+              </Text>
+            </Tooltip>
           )}
-          {(isLoomVideo || isZoomVideo || isGoogleVideo) && (
-            <iframe
-              src={project.video}
-              allowFullScreen
-              style={{
-                width: "100%",
-                height: "auto",
-                borderRadius: theme.radius.md,
-              }}
-            />
+          {teamMemberNames && (
+            <Box mt="auto" w="100%">
+              <Text size="sm">Team Members: </Text>
+              <Flex wrap={"wrap"}>
+                {teamMemberNames.map((name, index) => {
+                  if (index > 1) return null;
+                  return (
+                    <Flex w="70%" key={index} mb="0.3em">
+                      <Text size="xs" c="blue.6">
+                        {name}
+                      </Text>
+                      <ActionIcon
+                        color="blue.2"
+                        size={"sm"}
+                        ml="xs"
+                        onClick={() => {
+                          window.open(
+                            project.linkedIn.split("+")[index].replace(" ", ""),
+                            "_blank"
+                          );
+                        }}
+                      >
+                        <IconBrandLinkedin />
+                      </ActionIcon>{" "}
+                    </Flex>
+                  );
+                })}
+              </Flex>
+            </Box>
           )}
-        </Card.Section>
-        <Flex mt="md" mb="xs" w="100%">
-          <Text w={500}>{project.projectName ?? "Portfolio Project"}</Text>
-          {/* <Badge color="green.7" variant="light" w="60px">
-            C{project.cohort}
-          </Badge> */}
-        </Flex>
-        {project.projectDescription && (
-          <Tooltip
-            label={project.projectDescription.split("+").join(", ")}
-            withArrow
-            multiline
-            w={420}
-          >
-            <Text truncate mt="lg" size="sm" c="dimmed">
-              {project.projectDescription.split("+").join(", ")}
-            </Text>
-          </Tooltip>
-        )}
-        {teamMemberNames && (
-          <Box mt="auto" w="100%">
-            <Text size="sm">Team Members: </Text>
-            <Flex wrap={"wrap"}>
-              {teamMemberNames.map((name, index) => {
-                if (index > 1) return null;
-                return (
-                  <Flex w="70%" key={index} mb="0.3em">
-                    <Text size="xs" c="blue.6">
-                      {name}
-                    </Text>
-                    <ActionIcon
-                      color="blue.2"
-                      size={"sm"}
-                      ml="xs"
-                      onClick={() => {
-                        window.open(
-                          project.linkedIn.split("+")[index].replace(" ", ""),
-                          "_blank"
-                        );
-                      }}
-                    >
-                      <IconBrandLinkedin />
-                    </ActionIcon>{" "}
-                  </Flex>
-                );
-              })}
-            </Flex>
-          </Box>
-        )}
-        {/* </ScrollArea> */}
-      </Card>
+          {/* </ScrollArea> */}
+        </Card>
+      </motion.div>
     </Box>
   );
 };
